d4715e2bf23774e19f962484ede0ef53c6e47d98
Andrew Richter <andrewrichter1@hotmail.com>
2025-06-28 19:03:11 -0300

    user creation almost done

--- src/services/users/CreateUserService.ts
+++ src/services/users/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from "../../prisma";
 
 interface UserRequest {
     name: string;
@@ -7,8 +8,37 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
-        console.log(name);
-        return { ok: true }
+        
+        // Check if email was sent
+        if(!email){
+            throw new Error('Email incorrect.')
+        }
+
+        // Check if email is already registrated
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error('User with this email already registered.')
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user;
     }
 }
 
